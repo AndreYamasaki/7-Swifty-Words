73c2c28177353454798a01258b81e76b28ab6318
Andre <katsuya.tsu@gmail.com>
2021-07-21 11:24:16 -0300

    added labels, textField and buttons using code

--- Project8/ViewController.swift
+++ Project8/ViewController.swift
@@ -8,6 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - Attributes
+    
+    var cluesLabel: UILabel!
+    var answersLabel: UILabel!
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
